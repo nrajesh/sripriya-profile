62b7807c8cc1c9e2ca5878e72670f6f7e9d991e8
[dyad] <Narayanan.Rajesh@outlook.com>
2025-10-15 16:08:03 +0200

    Updated src/lib/data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -123,9 +123,9 @@ const rawBooks: Book[] = [
     publicationDate: "September 25, 2023",
     pageCount: "456",
     isbn: "9788196159078 (ISBN); B0C2YTW8SR (ASIN)",
-    category: "Translated Work",
-    tags: "memories, family, science, research, space, India, isro",
-    description: "This book is an Translated Work of the Tamil book ‘Ninaivugalukku Maranamillai’. Written by two people closest to A.P.J. Abdul Kalam—his niece Dr. Nazema Maraikayar and the distinguished ISRO scientist Dr. Y.S. Rajan, who was a close confidante of Kalam —this book gives a holistic and honest revelation of the life of Dr. Kalam from his early childhood till he breathed his last.\n\nThis is the story of how a small-town boy from Rameswaram ascended to the highest echelons of the Indian political world. This book comprehensively covers the beautiful history of Indian rocketry, precursors to today’s Science and Technology, the workings of the Indian political and administrative",
+    category: "Original Work",
+    tags: "religion, hinduism, India, scholar, guru, vedanta desikan, swamy",
+    description: "சுவாமி தேஸிகனின் 755 ஆவது திருநக்ஷத்ர மஹோத்ஸவத்தில் ஸ்ரீமத் ஆண்டவன் ஸ்ரீவராஹ மஹா தேசிகன் திருக்கரங்களால் ரிஷிகேஷ் ஆச்ரமத்தில் வெளியிடப்பட்டது.",
   },
   {
     title: "Kalam’s Family Tree",
