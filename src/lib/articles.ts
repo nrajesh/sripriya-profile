03c626c768cd6ecc5f54d4c1af591789c57d6df6
[dyad] <Narayanan.Rajesh@outlook.com>
2025-11-01 16:44:21 +0100

    Updated src/lib/articles.ts

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -7,6 +7,10 @@ export interface Article {
 // You can easily add new articles to this list.
 export const articlesData: Article[] = [
   {
+    name: "சிவ-ராம-கிருஷ்ணன்: மூன்று தத்துவங்கள்",
+    link: "https://www.prekshaa.in/%E0%AE%9A%E0%AE%BF%E0%AE%B5-%E0%AE%B0%E0%AE%BE%E0%AE%AE-%E0%AE%95%E0%AE%BF%E0%AE%B0%E0%AF%81%E0%AE%B7%E0%AF%8D%E0%AE%A3%E0%AE%A9%E0%AF%8D-%E0%AE%AE%E0%AF%82%E0%AE%A9%E0%AF%8D%E0%AE%B1%E0%AF%81-%E0%AE%A4%E0%AE%A4%E0%AF%8D%E0%AE%A4%E0%AF%81%E0%AE%B5%E0%AE%99%E0%AF%8D%E0%AE%95%E0%AE%B3%E0%AF%8D",
+  },
+  {
     name: "உபநயனம் – ஒரு எளிய அறிமுகம் (முகவுரை)",
     link: "https://www.prekshaa.in/upanayanam-part1-mugavurai",
     previewImageUrl: "/covers/upanayanam.jpg",
@@ -15,10 +19,6 @@ export const articlesData: Article[] = [
     name: "சமஸ்கிருதம் கற்றலும் நம் கலாச்சாரத்தின் மதிப்பும் (பகுதி 1)",
     link: "https://www.prekshaa.in/the-value-of-sanskrit-learning-and-culture-part1",
     previewImageUrl: "/covers/hiriyanna-collage.jpg",
-  },
-  {
-    name: "சிவ-ராம-கிருஷ்ணன்: மூன்று தத்துவங்கள்",
-    link: "https://www.prekshaa.in/%E0%AE%9A%E0%AE%BF%E0%AE%B5-%E0%AE%B0%E0%AE%BE%E0%AE%AE-%E0%AE%95%E0%AE%BF%E0%AE%B0%E0%AF%81%E0%AE%B7%E0%AF%8D%E0%AE%A3%E0%AE%A9%E0%AF%8D-%E0%AE%AE%E0%AF%82%E0%AE%A9%E0%AF%8D%E0%AE%B1%E0%AF%81-%E0%AE%A4%E0%AE%A4%E0%AF%8D%E0%AE%A4%E0%AF%81%E0%AE%B5%E0%AE%99%E0%AF%8D%E0%AE%95%E0%AE%B3%E0%AF%8D",
   },
   {
     name: "A Bird's Eye View of the Rāmāyaṇa and the Mahābhārata",
