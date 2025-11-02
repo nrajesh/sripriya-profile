cf8315e74c688ea8095cf649c69d00d3a847293d
[dyad] <Narayanan.Rajesh@outlook.com>
2025-11-02 10:10:48 +0100

    Updated src/lib/articles.ts

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -58,7 +58,7 @@ export const articlesData: Article[] = [
   },
   {
     name: "Thirukkazhukkundram Vedagireeswarar-Sokkanayagi temple",
-    link: "https://timeless-footprints.blogspot.com/2025/11/thirukkazhukkundram-vedagireeswarar.html",
+    link: "https://timeless-footprints.blogspot.com/2025/11/kanchipuram-varadaraja-perumal-temple.html",
     previewImageUrl: "/covers/thirukkazhukkundram-temple.jpg",
   },
 ];
